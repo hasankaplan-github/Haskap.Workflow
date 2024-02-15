43b4535251214a028f4f99e6812dc19bdce414c3
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2024-02-15 16:41:01 +0300

    ...

--- Haskap.Workflow.Domain/UserAggregate/UserDomainService.cs
+++ Haskap.Workflow.Domain/UserAggregate/UserDomainService.cs
@@ -12,17 +12,17 @@ using System.Threading.Tasks;
 namespace Haskap.Workflow.Domain.UserAggregate;
 public class UserDomainService : DomainService
 {
-    private readonly IWorkflowDbContext _recipeDbContext;
+    private readonly IWorkflowDbContext _workflowDbContext;
 
-    public UserDomainService(IWorkflowDbContext recipeDbContext)
+    public UserDomainService(IWorkflowDbContext workflowDbContext)
     {
-        _recipeDbContext=recipeDbContext;
+        _workflowDbContext = workflowDbContext;
     }
 
     public async Task<HashSet<string>> GetAllPermissionsAsync(Guid userId, CancellationToken cancellationToken)
     {
-        var allPermissions = (from user in _recipeDbContext.User.Include(x => x.Roles).Where(x => x.Id == userId)
-                              from role in _recipeDbContext.Role.Where(x => user.Roles.Select(y => y.RoleId).Contains(x.Id)).DefaultIfEmpty()
+        var allPermissions = (from user in _workflowDbContext.User.Include(x => x.Roles).Where(x => x.Id == userId)
+                              from role in _workflowDbContext.Role.Where(x => user.Roles.Select(y => y.RoleId).Contains(x.Id)).DefaultIfEmpty()
                               select role.Permissions.Select(x => x.Name).Concat(user.Permissions.Select(x => x.Name)))
                               .SelectMany(x => x)
                               .ToHashSet();
@@ -32,7 +32,7 @@ public class UserDomainService : DomainService
 
     public async Task<HashSet<string>> GetUserPermissionsAsync(Guid userId, CancellationToken cancellationToken)
     {
-        var user = await _recipeDbContext.User
+        var user = await _workflowDbContext.User
            .Where(x => x.Id == userId)
            .FirstAsync(cancellationToken);
 
@@ -45,9 +45,9 @@ public class UserDomainService : DomainService
 
     public async Task<HashSet<string>> GetRolePermissionsAsync(Guid userId, CancellationToken cancellationToken)
     {
-        var rolePermissions = (from user in _recipeDbContext.User
-                               join userRole in _recipeDbContext.UserRole on user.Id equals userRole.UserId
-                               join role in _recipeDbContext.Role on userRole.RoleId equals role.Id
+        var rolePermissions = (from user in _workflowDbContext.User
+                               join userRole in _workflowDbContext.UserRole on user.Id equals userRole.UserId
+                               join role in _workflowDbContext.Role on userRole.RoleId equals role.Id
                                where user.Id == userId
                                select role.Permissions.Select(x => x.Name))
                             .SelectMany(x => x)
