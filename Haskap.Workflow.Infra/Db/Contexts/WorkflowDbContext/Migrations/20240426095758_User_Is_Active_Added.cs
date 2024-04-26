30b28ad880886378b9573c7436717ff2491700d9
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2024-04-26 13:00:05 +0300

    DddBase User.IsActive ve Tenant eklenmiş hali alındı.

---
+++ Haskap.Workflow.Infra/Db/Contexts/WorkflowDbContext/Migrations/20240426095758_User_Is_Active_Added.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Haskap.Workflow.Infra.Db.Contexts.WorkflowDbContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class User_Is_Active_Added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "user",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "tenant_id",
+                table: "user",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "tenant",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_tenant", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tenant");
+
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "tenant_id",
+                table: "user");
+        }
+    }
+}
